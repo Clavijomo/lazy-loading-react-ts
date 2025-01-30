febb263ae2a757f588e686b7983156beb09c4148
Clavijomo <jonathanclavijo0907@gmail.com>
2025-01-30 16:04:18 -0500

    :rocket: Initial Project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="flex justify-center items-center h-60">
+            <h1 className="text-4xl">Lazy Loading</h1>
+        </div>
+    )
+}
+
+export default App;
